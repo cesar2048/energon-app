1e9fff88bee484e6a9ea4a20212795ada36c751a
Cesar <cesar.reyes10@gmail.com>
2020-07-21 20:42:21 -0600

    type codigo

---
+++ components/Camera.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+} from 'react-native';
+import {
+    Camera
+} from 'expo-camera';
+
+const CameraView = () => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <Text>No se nada</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={{ flex: 1 }}>
+            <Camera style={{ flex: 1 }} type={type}>
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'transparent',
+                        flexDirection: 'row',
+                    }}>
+                    <TouchableOpacity
+                        style={{
+                            flex: 0.1,
+                            alignSelf: 'flex-end',
+                            alignItems: 'center',
+                        }}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}>
+                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
+    )
+}
+
+export default CameraView;
