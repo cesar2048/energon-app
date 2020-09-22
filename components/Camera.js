0f8cf91e4290640a010368a687da9ddc8233fd84
Cesar <cesar.reyes10@gmail.com>
2020-09-21 22:01:28 -0600

    Adding ws connection

--- components/Camera.js
+++ components/Camera.js
@@ -15,7 +15,7 @@ async function alertIfRemoteNotificationsDisabledAsync() {
     }
 }
 
-const URL = 'http://192.168.2.72:8080/upload';
+const URL = 'http://192.168.2.76:8080/upload';
 
 const uploadFile = async (path) => {
     const data = new FormData();
@@ -36,83 +36,135 @@ const uploadFile = async (path) => {
         }
     });
     
-    let response = await res.text();
-    if (response.status === 1) {
-        alert(response.data);
-    } else {
-        alert('Error')
-    }
+    let response = await res.json();
+    alert(JSON.stringify(response));
 };
 
-const CameraView = () => {
-    let camRef = useRef(null);
-    const [hasPermission, setHasPermission] = useState(null);
-    const [mode, setMode] = useState('stopped');
-    const [type, setType] = useState(Camera.Constants.Type.back);
+class HandlerServer {
+    constructor(host, controller) {
+        this.host = host;
+        this.started = false;
+        this.controller = controller;
+    }
 
-    useEffect(() => {
-        (async () => {
+    start() {
+        if (this.started) {
+            return;
+        }
+
+        this.started = true;
+        this.ws = new WebSocket(`ws://${this.host}/path`);
+        this.ws.onopen = () => {
+            alert('connected to server');
+        }
+        this.ws.onmessage = (e) => {
+            if (e.data == 'Start recording') {
+                this.controller.startRecording();
+            } else if (e.data == 'Stop recording') {
+                this.controller.stopRecording();
+            } else {
+                alert(`Unknown message: ${e.data}`);
+            }
+        }
+        this.ws.onerror = (e) => {
+            alert(`Ws:Error, ${e.message}`);
+        }
+    }
+}
+
+
+class CameraView extends React.Component {
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            mode: 'stopped',
+            hasPermission: false,
+        }
+        this.camRef = React.createRef();
+        this.srv = new HandlerServer('192.168.2.76:8080', this);
+    }
+
+    componentDidMount() {
+        const fn = async () => {
             const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
             await alertIfRemoteNotificationsDisabledAsync();
-        })();
-    }, []);
-
-
-    if (hasPermission === null) {
-        return <Text>No se nada</Text>;
-    }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-    }
-
-    const record = () => {
-        if (mode === 'stopped') {
-            camRef.recordAsync().then(data => {
-                alert(JSON.stringify(data, null, 4));
-                uploadFile(data.uri);
-            }).catch(err => {
-                alert(JSON.stringify(err, null, 4));
+            this.setState({
+                hasPermission: status === 'granted',
             });
-            setMode('recording');
+        }
+        fn();
+        this.srv.start();
+    }
+
+    startRecording() {
+        const options = {
+            quality: '1080p',
+        };
+        this.camRef.recordAsync(options).then(data => {
+            alert(JSON.stringify(data, null, 4));
+            uploadFile(data.uri);
+        }).catch(err => {
+            alert(JSON.stringify(err, null, 4));
+        });
+    }
+
+    stopRecording() {
+        this.camRef.stopRecording();
+    }
+
+    toggleRecording() {
+        if (mode === 'stopped') {
+            this.startRecording();
+            this.setState({ mode: 'recording' });
         }
         if (mode === 'recording') {
-            camRef.stopRecording();
-            setMode('stopped');
+            this.stopRecording();
+            this.setState({ mode: 'stopped' });
         }
-    };
+    }
 
-    return (
-        <View style={{ flex: 1, borderColor: '#0F0', borderWidth: 1 }}>
-            <Camera style={{ flex: 15 }} type={type} ref={(ref) => camRef = ref} >
-                <View
-                    style={{
-                        flex: 1,
-                        backgroundColor: 'transparent',
-                        flexDirection: 'row',
-                    }}>
-                    <TouchableOpacity
+    render() {
+        const type = Camera.Constants.Type.back;
+        const { hasPermission, mode } = this.state;
+
+        if (hasPermission === null) {
+            return <Text>No se nada</Text>;
+        }
+        if (hasPermission === false) {
+            return <Text>No access to camera</Text>;
+        }
+        return (
+            <View style={{ flex: 1, borderColor: '#0F0', borderWidth: 1 }}>
+                <Camera style={{ flex: 15 }} type={type} ref={(ref) => this.camRef = ref} >
+                    <View
                         style={{
-                            flex: 0.1,
-                            alignSelf: 'flex-end',
-                            alignItems: 'center',
-                        }}
-                        onPress={() => {
-                            setType(
-                                type === Camera.Constants.Type.back
-                                    ? Camera.Constants.Type.front
-                                    : Camera.Constants.Type.back
-                            );
+                            flex: 1,
+                            backgroundColor: 'transparent',
+                            flexDirection: 'row',
                         }}>
-                        <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                                flex: 0.1,
+                                alignSelf: 'flex-end',
+                                alignItems: 'center',
+                            }}
+                            onPress={() => {
+                                setType(
+                                    type === Camera.Constants.Type.back
+                                        ? Camera.Constants.Type.front
+                                        : Camera.Constants.Type.back
+                                );
+                            }}>
+                            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                        </TouchableOpacity>
+                    </View>
+                </Camera>
+                <View style={{ flex: 1 }}>
+                    <Button onPress={() => this.toggleRecording()} title={mode === 'stopped' ? 'Grabar' : 'Detener'} />
                 </View>
-            </Camera>
-            <View style={{ flex: 1 }}>
-                <Button onPress={record} title={mode === 'stopped' ? 'Grabar' : 'Detener'} />
             </View>
-        </View>
-    )
+        );
+    }
 }
 
 export default CameraView;
